af30b8ed902ab057e21d5176f0a7e2e2e9349eb9
adamking2424 <adam.king.297@gmail.com>
2020-09-23 14:05:10 +0200

    All thumbnails are now being replaced. Need to add the other URL options, aswell as check for 404

--- js/background.js
+++ js/background.js
@@ -4,36 +4,46 @@
 
 'use strict';
 
+
+let error404Listener = null;
+let preferredThumbnailFile = 'hq1';
+let noRedirectToken = 'zctf420otaqimwn9lx8m';
+
 chrome.runtime.onInstalled.addListener(function() {
   chrome.storage.sync.set({color: '#3aa757'}, function() {
     console.log("The color is green.");
    
   })
 
+  //TODO: add checking to replace in the case that the original string is in different format
+  let redirectListener = function (details) {
+    details.url = details.url.replace('hq720', preferredThumbnailFile);
+    return {redirectUrl: details.url};
+  };
+  let filter = {
+          urls: [
+            'https://i.ytimg.com/vi/*/default.jpg*',
+            'https://i.ytimg.com/vi/*/hqdefault.jpg*',
+            'https://i.ytimg.com/vi/*/mqdefault.jpg*',
+            'https://i.ytimg.com/vi/*/sddefault.jpg*',
+            'https://i.ytimg.com/vi/*/hq720.jpg*',
+            'https://i.ytimg.com/vi_webp/*/default.webp*',
+            'https://i.ytimg.com/vi_webp/*/hqdefault.webp*',
+            'https://i.ytimg.com/vi_webp/*/mqdefault.webp*',
+            'https://i.ytimg.com/vi_webp/*/sddefault.webp*'
+          ]
+      };
+  let opt_extraInfoSpec = ['blocking'];
 
-  //TODO -> need work this out 
+  setupThumbnailRedirectors();
+
+  function setupThumbnailRedirectors () {
+    chrome.webRequest.onBeforeRequest.addListener(redirectListener, filter,opt_extraInfoSpec);
+  }
   
-  // chrome.webRequest.onBeforeRequest.addListener(
-  //   redirectListener = function (details) {
-  //       if (!details.url.includes(`&noRedirectToken=${noRedirectToken}`)) {
-  //           if (details.url.startsWith('https://i.ytimg.com/vi/')) {
-  //               return {redirectUrl: details.url.replace(/(default|hqdefault|mqdefault|sddefault|hq720).jpg/, `${preferredThumbnailFile}.jpg`)};
-  //           } else if (details.url.startsWith('https://i.ytimg.com/vi_webp/')) {
-  //               return {
-  //                   redirectUrl: details.url.replace(/(default|hqdefault|mqdefault|sddefault).webp.*/, `${preferredThumbnailFile}.jpg`)
-  //                       .replace('/vi_webp/', '/vi/')
-  //               };
-  //           }
-  //       }
-  //   });
-    
-  
-
-
   chrome.tabs.query({url: '*://www.youtube.com/*'}, function (tabs) {
     tabs.forEach(function (tab) {
-      console.log('foreach');
-      console.log(tab);
+      console.log('foreachtab', tab);
         chrome.tabs.executeScript(tab.id, {file: 'js/youtube.js'}, function () {
           console.log("This is being called");
             chrome.tabs.sendMessage(tab.id,"meessa");
--- js/youtube.js
+++ js/youtube.js
@@ -1,21 +1,9 @@
 chrome.runtime.onMessage.addListener(function (message) {
   console.log('onMessage-> ' + message);
-  // Object.keys(message).forEach(function (change) {
-  //     console.log('youtube.js onMessage()');
-  // })
+ 
 
-  var elements = document.getElementsByClassName("style-scope yt-img-shadow");
-  // elements.forEach(element => {
-  //   console.log(element);
-  //   element.src = 'img/get_started16.png';
-  // })
-  for (let i = 0; i < elements.length;i++) {
-    console.log(elements[i]);
-    let firstActualImage = '';
-    if (elements[i].src) {
-      console.log('Has a source');
-      elements[i].src = "https://i.ytimg.com/vi/OCdrRSt0ysc/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLDU93uO-MWyIIuri-DNXazphgKSnw";
-    }
-  }
+  //var elements = document.getElementsByClassName("style-scope yt-img-shadow");
+  
+
 
 });
