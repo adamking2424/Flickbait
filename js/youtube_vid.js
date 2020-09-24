6d6d000abbcbd8927f6f99d8edd4adb9e0a7c422
adamking2424 <adam.king.297@gmail.com>
2020-09-24 09:41:54 +0200

    Added rudimentary up/downvote to video page.

--- js/youtube_vid.js
+++ js/youtube_vid.js
@@ -1,17 +1,21 @@
 console.log('youtube_vid.js');
 
+buildVoteUI();
 
-let titleContainerElement = document.getElementsByTagName('ytd-video-primary-info-renderer')[0];
-
-
-console.log(titleContainerElement);
-console.log(titleContainerElement.childNodes[1].childNodes[5].childNodes[1]);
-
-titleContainerElement.childNodes[1].childNodes[5].childNodes[1].innerHTML = "<center>THIS MAYBE WHERE VOTING</center>";
-
-//'<button>Upvote</button><span>1</span><button>Downvote</button>'
-
-//"<center>THIS MAYBE WHERE VOTING</center>"
+//TODO need to clean this up, dont think it should creeate element then have to search for the same one.
+function buildVoteUI () {
+  let titleContainerElement = document.getElementsByTagName('ytd-video-primary-info-renderer')[0];
+  titleContainerElement.childNodes[1].childNodes[5].childNodes[1].innerHTML = '<button id=12>up</button><span id=13>1</span><button id=14>down</button>';
+  let counterSpan = document.getElementById('13');
+  document.getElementById('12').addEventListener('click',function(){
+    console.log('upvoting');
+    counterSpan.innerHTML = Number(counterSpan.innerHTML) + 1;
+  });
+  document.getElementById('14').addEventListener('click',function(){
+    console.log('downvoting');
+    counterSpan.innerHTML = Number(counterSpan.innerHTML) - 1;
+  });
+}
 
 
 
